184a0e0f63a8c3b6c5097b1f73a3daed8b381ceb
SiroBrt <sirobroton@gmail.com>
2023-02-01 19:33:02 +0100

    en casita :>

--- ex 1.9.cpp
+++ ex 1.9.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 int main (){
-    int num[3];
-    for (int i=0; i<=2; i++){
+    int num[2],min,max,mid;
+    cin >>mid;
+    max=mid;
+    min=mid;
+    for (int i=0; i<=1; i++){
         cin >>num[i];
+        if (num[i]>max){
+            max=num[i];
+        }else if(num[i]<min){
+            min=num[i];
+        }else{
+            mid=num[i];
+        }
     }
+    cout <<max <<" " <<mid <<" " <<min;
 }
