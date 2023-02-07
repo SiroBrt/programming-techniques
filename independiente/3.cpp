d9a7f68f8d78827f5292812703777cd524182897
SiroBrt <sirobroton@gmail.com>
2023-02-07 21:19:13 +0100

    3

--- independiente/3.cpp
+++ independiente/3.cpp
@@ -2,17 +2,17 @@
 #include <cmath>
 using namespace std;
 /*The prime factors of 13195 are 5, 7, 13 and 29.
-What is the largest prime factor of the number 600851475143 ?*/
+What is the largest prime factor of the number 600851475143?*/
 
-double bignum=600851475143,num,factor;
+long long int bignum=600851475143,num,factor;
 
 int main(){
     num=bignum;
-    for(double i=2; i<=sqrt(bignum);i++){
+    for(long long int i=2; i<=sqrt(bignum);i++){
         while (num%i==0){
             num=num/i;
             factor=i;
-            cout <<factor;
+            cout <<"i=" <<i <<" num=" <<num <<endl;
         }
     }
     cout <<factor;
