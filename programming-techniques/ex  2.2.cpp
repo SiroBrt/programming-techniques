ea3a153a4f1f94ec9b51add2aa3f3542900d4bb0
SiroBrt <sirobroton@gmail.com>
2023-02-08 11:45:41 +0100

    ex 2

--- programming-techniques/ex  2.2.cpp
+++ programming-techniques/ex  2.2.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
-
+int tamano=5;
 int main(){
-    int num,v[20];
-    for (int i=19;i>0;i++){
+    int num,v[tamano];
+    for (int i=tamano-1;i>=0;i--){
         cin >>num;
         if (num==0){
-            break
+            break;
         }else{
             v[i]=num;
         }
     }
-    for 
+    cout <<"printing \r";
+    for(int j=0;j<=tamano-1;j++){
+        cout <<v[j] <<" ";
+    }
 }
---
+++ programming-techniques/ex 2.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector <int> v;
+    int num;
+    for (int i=0;i<10;i++){
+        cin >>num;
+        v.push_back(num);
+    }
+    for (int i=0;i<v.size();i++){
+        cout <<v[i] <<" ";
+    }
+}
