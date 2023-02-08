a9f740e8077d1c030620a663b3191bf7848b8cd8
SiroBrt <sirobroton@gmail.com>
2023-02-08 11:32:55 +0100

    2

--- programming-techniques/ex  2.2.cpp
+++ programming-techniques/ex  2.2.cpp
@@ -5,6 +5,11 @@ int main(){
     int num,v[20];
     for (int i=19;i>0;i++){
         cin >>num;
-
+        if (num==0){
+            break
+        }else{
+            v[i]=num;
+        }
     }
+    for 
 }
