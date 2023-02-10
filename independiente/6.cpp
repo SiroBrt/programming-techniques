13f28972651d293e09ec3f765501df9ce67ea435
SiroBrt <sirobroton@gmail.com>
2023-02-10 12:23:07 +0100

    6

---
+++ independiente/6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+/*The sum of the squares of the first ten natural numbers is 385
+The square of the sum of the first ten natural numbers is 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 2640
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.*/
+
+int n=100,sqsum,sumsq;
+
+int main(){
+    sqsum=pow(n*(n+1)/2,2);
+    sumsq=(n*(n+1)*(2*n+1))/6;
+    cout <<sqsum-sumsq;
+
+}
