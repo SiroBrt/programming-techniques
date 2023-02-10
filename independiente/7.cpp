749301ffe6ee20c34f3dc640b699379b239b81bc
SiroBrt <sirobroton@gmail.com>
2023-02-10 12:51:03 +0100

    7 intento

---
+++ independiente/7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+/*By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?*/
+
+int count,index=6,num;
+vector <int> primes={1};
+
+int main(){
+    count=1;
+    while(count<=index){
+        for(int i=0;i<primes.size();i++){
+            
+        }
+
+    }
+}
