708d74debcb730a440b1a9f3e7f5f416faedaf4a
SiroBrt <sirobroton@gmail.com>
2023-02-12 19:12:51 +0100

    intento 100923

---
+++ independiente/num_to_char_Array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+char a[1000];
+int maximo,product,packSize=4;
+
+int main(){
+    cin >>a;
+    for(int i=0;i<1000;i++){
+        cout <<a[i] <<",";
+    }
+}
