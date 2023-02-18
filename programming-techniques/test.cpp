a7c0128f26aa31d7eed2e7060f1418a5accd5fd2
SiroBrt <sirobroton@gmail.com>
2023-02-18 19:35:26 +0100

    organizacion

--- programming-techniques/test.cpp
+++ programming-techniques/test.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 using namespace std;
 
 int main(){
-    string str="1234567890";
-    int num;
-    for(int i=0;i<10;i++){
-        num=stoi(str.substr(i,2));
-        cout <<num <<endl;
-    }
+
 }
