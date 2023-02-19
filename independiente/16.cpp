3578c47d41fbdee83a0046b9ee58c2bdd051d3f6
SiroBrt <sirobroton@gmail.com>
2023-02-19 12:58:31 +0100

    16

--- independiente/16.cpp
+++ independiente/16.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 /*2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-What is the sum of the digits of the number 21000?*/
+What is the sum of the digits of the number 2^1000?*/
 int exponent=1000,aux,sum=0;
 
 int sumarDigitos(int input){
