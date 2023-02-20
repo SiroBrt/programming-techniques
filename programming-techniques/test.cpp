75e9052bebcaf93a824a86b13365a1bd270f1055
SiroBrt <sirobroton@gmail.com>
2023-02-20 10:55:03 +0100

    18

--- programming-techniques/test.cpp
+++ programming-techniques/test.cpp
@@ -2,6 +2,20 @@
 #include <cmath>
 using namespace std;
 
+string toBinary(int n){
+    string r;
+    while(n!=0){
+        r=(n%2==0 ?"0":"1")+r;
+        n/=2;
+    }
+    cout <<"r.length=" <<r.length() <<" r=" <<r <<endl;
+    return r;
+}
+
 int main(){
+    int num;
+    num=0;
+    auto binary=toBinary(num);
 
 }
+//11111111111111
