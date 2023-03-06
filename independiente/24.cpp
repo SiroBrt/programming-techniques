02d54c050418e1dcd0aa9ccbc0efcd19c65e3d5e
SiroBrt <sirobroton@gmail.com>
2023-03-06 20:57:56 +0100

    test

---
+++ independiente/24.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+int elementos=3;
+
+int main(){
+    for(int i=0;i<elementos;i++){
+        
+    }
+}
--- programming-techniques/test.cpp
+++ programming-techniques/test.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+class Shape{
+public:
+    float ar,prmtr;
+    float getArea(){
+        return ar;
+    }
+    float getPerimeter(){
+        return prmtr;
+    }
+    Shape(){
+        ar=prmtr=0;
+    }
+};
+class Square:public Shape{
+    public:
+    float side;
+    Square(){
+        ar=prmtr=side=0;
+    }
+    Square(float l){
+        side=l;
+        ar=l*l;
+        prmtr=4*l;
+    }
+};
+class Triangle:public Shape{
+    public:
+    float base,height,hyp;
+    Triangle(){
+        ar=prmtr=base=height=hyp=0;
+    }
+    Triangle(float b,float h){
+        base=b;
+        height=h;
+        hyp=sqrt(pow(base,2)+pow(height,2));
+        ar=base*height/2;
+        prmtr=base+height+hyp;
+    }
+};
 
 int main(){
-    int num;
-    num=105;
-    cout <<(num<100 ?"0":"1");
+    float num1,num2;
+    cout <<"introduce the base: ";
+    cin >>num1;
+    cout <<"introduce the height (for triangle): ";
+    cin >>num2;
+    Triangle t1(num1,num2);
+    Square s1(num1);
+    cout <<"area square=" <<s1.getArea() <<"\nperimeter square=" <<s1.getPerimeter() <<"\narea triangle=" <<t1.getArea() <<"\nperimeter triangle=" <<t1.getPerimeter() ;
 }
