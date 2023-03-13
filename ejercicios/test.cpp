35e5586e0beb425383c01f59c081ee94b3c4cbaf
SiroBrt <sirobroton@gmail.com>
2023-03-13 11:16:27 +0100

    a

--- ejercicios/test.cpp
+++ ejercicios/test.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 using namespace std;
 
+static vector <pair <pair <string,string>,vector <pair <int,string>>>> Wagons;
 
 int main(){
-    float num1,num2;
-    cout <<"introduce number 1: ";
-    cin >>num1;
-    cout <<"introduce number 2: ";
-    cin >>num2;
-    cout <<(num1>10? "":"0") <<num1 <<" and " <<(num2>10? "":"0") <<num2;
+    pair <string,string> sitio;
+    pair <int,string> persona1,persona2;
+    vector <pair <int,string>> pasajeros;
+    pair <pair <string,string>,vector <pair <int,string>>> vagonPrueba;
+    sitio.first="leganes";
+    sitio.second="madrid";
+    persona1.first=1;
+    persona1.second="ID1";
+    persona2.first=2;
+    persona2.second="ID2";
+    pasajeros.push_back(persona1);
+    pasajeros.push_back(persona2);
+    vagonPrueba.first=sitio;
+    vagonPrueba.second=pasajeros;
+    Wagons.push_back(vagonPrueba);
+    cout <<Wagons[0].second[1].second;
 }
